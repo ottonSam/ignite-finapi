bf67179dfd9d445d2d90674e18b87fee7af3225c
ottonsam <ottonsam@gmail.com>
2021-10-05 14:14:46 -0300

    Using middleware to verify the account exists

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) {
+    return res.status(400).json({ error: "Customer not found!" });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body;
 
@@ -28,15 +42,9 @@ app.post('/account', (req, res) => {
 
 });
 
-app.get('/statement', (req, res) => {
-  const { cpf } = req.headers;
-
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if(!customer) {
-    return res.status(400).json({ error: "Customer not found!" });
-  }
-
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  
   return res.json(customer.statement);
 });
 
